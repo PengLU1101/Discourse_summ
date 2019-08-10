ca8a64e9f910078d971a831c81708319b5502eca
PengLU1101 <lpxd1101@outlook.com>
2019-08-10 16:04:16 -0400

    fix error

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
-import pickle, json
+import pickle, json, os
 import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.probability import FreqDist
@@ -16,7 +16,7 @@ path = '/u/lupeng/Project/code/OpenNMT-py/data/cnndm/cnndm'
 
 
 def get_sent_list(path_r, path_w):
-    if os.isfile(path_w):
+    if os.path.isfile(path_w):
         with open(path_w, 'r') as f:
             doc_list = pickle.load(f)
     else:
@@ -27,24 +27,26 @@ def get_sent_list(path_r, path_w):
         #    print(i, "<s>" + x + "<\s>")
         #print(len(token_txt))
         #print(line)
-        doc_list = []
-        i = 1
-        for line in tqdm(f.readlines()):
-            token_sents = sent_tokenize(line)
-            doc = " ".join([x + "<\s>" for x in token_sents])
-            doc_list.append(word_tokenize(doc))
-            if i == 1:
-                print(word_tokenize(doc))
-                i = 0
-        with open(path_w, 'w') as f_w:
-            print("start writing...")
-            pickle.dump(doc_list, f_w)
+            doc_list = []
+            i = 1
+            for line in tqdm(f.readlines()):
+                token_sents = sent_tokenize(line)
+                doc = " ".join([x + "<s>" for x in token_sents])
+                doc_list.append(word_tokenize(doc))
+                if i == 1:
+                    lll = word_tokenize(doc)
+                    print(lll)
+                    i = 0
+                    print(len(lll))
+            with open(path_w, 'w') as f_w:
+                print("start writing...")
+                pickle.dump(doc_list, f_w)
     return doc_list
 
 
 def get_freq_dict(doc_list):
     freq_dict = defaultdict(int)
-    print(Getting freq_dict...)
+    print('Getting freq_dict...')
     for doc in tqdm(doc_list):
         for word in doc:
             freq_dict[word] += 1
@@ -57,6 +59,7 @@ def get_freq_dict(doc_list):
             count_freq50 += 1
     print("freq < 5", count_freq5/len(freq_dict))
     print("freq < 50", count_freq50/len(freq_dict))
+    return freq_dict
 
 
     #with open(path+'/train.txt.src_with_sent', 'w') as f_w:
@@ -68,7 +71,8 @@ def main():
     path_r = path + '/train.txt.src'
     path_w = path + '/train_sent_list'
 
-
+    l = get_sent_list(path_r, path_w)
+    f = get_freq_dict(l)
 
 if __name__ == '__main__':
     main()
