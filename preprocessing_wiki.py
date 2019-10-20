73958d86558c52dee8177ced5dac77812d54cabb
PengLU1101 <lpxd1101@outlook.com>
2019-10-20 18:15:03 -0400

    add pre for wiki

---
+++ preprocessing_wiki.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# author：Peng time:2019-10-20
+
+import json
+import os
+import pickle
+import re
+import random
+from itertools import chain
+
+def make_json(path_data):
+    
+
+def test():
+    pass
+
+
+if __name__ == "__main__":
+    test()
