e40e6874e903a0ec564c282914a681bcb5439313
PengLU1101 <lpxd1101@outlook.com>
2019-10-28 11:39:12 -0400

    add finegrain func

--- preprocessing_wiki.py
+++ preprocessing_wiki.py
@@ -7,6 +7,7 @@ import os
 import random
 from itertools import chain
 import argparse
+import re
 
 from nltk.tokenize import sent_tokenize, word_tokenize
 from tqdm import tqdm
@@ -21,7 +22,7 @@ def make_json(path_data, split):
         if len(line) > 500:
             sentences = sent_tokenize(line)
             json_file = os.path.join(path_data, f'{split}/{save_id}.json')
-            if (len(sentences) > 5) and (len(sentences) < 50):
+            if (len(sentences) > 8) and (len(sentences) < 50):
                 new_data = {'src': sentences}
                 with open(json_file, 'w+') as fw:
                     json.dump(new_data, fw)
@@ -64,6 +65,24 @@ def add_neg(path, split, part=None, a=None, b=None):
         js['neg'] = neg_list
         with open(os.path.join(jsonfile_dir, f'{i}.json'), 'w+') as f:
             json.dump(js, f)
+def finegrain(path1, path2, split):
+    read_path = os.path.join(path1, split)
+    save_path = os.path.join(path2, split)
+    matcher = re.compile(r'[0-9]+\.json')
+    match = lambda name: bool(matcher.match(name))
+    names = os.listdir(read_path)
+    n_data = len(list(filter(match, names)))
+    idx = 0
+    for i in tqdm(range(n_data)):
+        with open(os.path.join(read_path, f"{i}.json")) as f:
+            js = json.loads(f.read())
+        _ = [len(x.lower().split()) for x in js['src']]
+        if max(_) < 50:
+            with open(os.path.join(save_path, f'{idx}.json'), "w+") as f:
+                json.dump(js, f)
+            idx += 1
+
+
 
 
 def check_files(path, split):
@@ -122,6 +141,8 @@ if __name__ == "__main__":
     parser.add_argument('--b', default=0, type=int)
     args = parser.parse_args()
     path = '/u/lupeng/Project/dataset/wikitext-103'
+    path2 = '/u/lupeng/Project/dataset/wikitext_103'
     #make_json(path, 'train')
     #add_neg(path, 'train', part=args.part)
-    check_files(path, 'train')
+    #check_files(path, 'train')
+    finegrain(path, path2, 'train')
