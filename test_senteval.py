59d2a60f1defaf8ecfce152ea29b86aebd48d5f1
PengLU1101 <lpxd1101@outlook.com>
2019-10-31 23:23:19 -0400

    update

--- test_senteval.py
+++ test_senteval.py
@@ -2,35 +2,45 @@
 # -*- coding: utf-8 -*-
 # author：Peng time:2019-10-28
 
-
+import logging
+import sys
+sys.setdefaultencoding('utf8')
 import senteval
+
+# Set PATHs
+PATH_TO_SENTEVAL = '../'
+PATH_TO_DATA = '../data/senteval_data/'
+PATH_TO_SKIPTHOUGHT = ''
 
 
 def prepare():
     pass
 
-def batcher():
-    pass
+def batcher(params, batch):
 
-params = {
+    return embeddings
+
+params_senteval = {
     'task_path': PATH_TO_DATA,
     'usepytorch': True,
     'kfold': 10
 }
 
-params['classifier'] = {
+params_senteval['classifier'] = {
     'nhid': 0,
     'optim': 'adam',
     'batch_size': 64,
     'tenacity': 5,
-    'epoch_size': 4}
-se = senteval.engine.SE(params, batcher, prepare)
-transfer_tasks = ['CR', 'MR', 'MPQA', 'SUBJ', 'SST2', 'SST5', 'TREC', 'MRPC', 'SNLI',
-                  'SICKEntailment', 'SICKRelatedness', 'STSBenchmark', 'ImageCaptionRetrieval',
-                  'STS12', 'STS13', 'STS14', 'STS15', 'STS16',
-                  'Length', 'WordContent', 'Depth', 'TopConstituents','BigramShift', 'Tense',
-                  'SubjNumber', 'ObjNumber', 'OddManOut', 'CoordinationInversion']
-results = se.eval(transfer_tasks)
+    'epoch_size': 4
+}
+
 
 if __name__ == "__main__":
-    test()
+    params_senteval['encoder'] = PEmodel.load_model()
+    se = senteval.engine.SE(params_senteval, batcher, prepare)
+    transfer_tasks = ['CR', 'MR', 'MPQA', 'SUBJ', 'SST2', 'SST5', 'TREC', 'MRPC', 'SNLI',
+                      'SICKEntailment', 'SICKRelatedness', 'STSBenchmark', 'ImageCaptionRetrieval',
+                      'STS12', 'STS13', 'STS14', 'STS15', 'STS16',
+                      'Length', 'WordContent', 'Depth', 'TopConstituents', 'BigramShift', 'Tense',
+                      'SubjNumber', 'ObjNumber', 'OddManOut', 'CoordinationInversion']
+    results = se.eval(transfer_tasks)
